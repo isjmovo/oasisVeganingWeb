80aaba750d2d945a3a35ac5108f86c07b75af5c6
isjmovo <isjmovo@naver.com>
2023-06-08 13:29:52 +0900

    MainController::index 메소드 추가

---
+++ src/main/java/com/ll/exam/UserService/MainController.java
@@ -0,0 +1,12 @@
+package com.ll.exam.UserService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+  @RequestMapping("/main")
+  public void index() {
+    System.out.println("index");
+  }
+}
