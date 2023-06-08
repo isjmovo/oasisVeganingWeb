e0b6a537101aeee089948e02300bdf4f987165a1
isjmovo <isjmovo@naver.com>
2023-06-08 13:35:55 +0900

    GetMapping

--- src/main/java/com/ll/exam/UserService/MainController.java
+++ src/main/java/com/ll/exam/UserService/MainController.java
@@ -1,14 +1,14 @@
 package com.ll.exam.UserService;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-  @RequestMapping("/main")
+  @GetMapping("/main")
   @ResponseBody
   public String index() {
-    return "안녕하세요.";
+    return "안녕하세요. UserService입니다.";
   }
 }
