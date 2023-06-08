545c7c99e17db5d1eda662180a7a0753a5a83026
isjmovo <isjmovo@naver.com>
2023-06-08 13:32:56 +0900

    ResponsBody

--- src/main/java/com/ll/exam/UserService/MainController.java
+++ src/main/java/com/ll/exam/UserService/MainController.java
@@ -2,11 +2,13 @@ package com.ll.exam.UserService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
   @RequestMapping("/main")
-  public void index() {
-    System.out.println("index");
+  @ResponseBody
+  public String index() {
+    return "안녕하세요.";
   }
 }
