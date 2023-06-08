3761f1fa0520b34e8a82edb597d0550e5ab52711
isjmovo <isjmovo@naver.com>
2023-06-08 13:16:37 +0900

    세팅

---
+++ src/main/java/com/ll/exam/UserService/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.UserService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/UserService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.UserService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
