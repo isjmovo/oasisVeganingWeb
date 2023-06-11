1bd8318f4798cdefd4fbf44cc21cc566168058e9
isjmovo <isjmovo@naver.com>
2023-06-11 18:11:27 +0900

    회원 테스트 케이스를 실행하기 전, 기존 데이터 삭제, 회원 2명 생성 후 진행

--- src/test/java/com/ll/exam/UserService/UserServiceTests.java
+++ src/test/java/com/ll/exam/UserService/UserServiceTests.java
@@ -1,6 +1,8 @@
 package com.ll.exam.UserService;
 
+import com.ll.exam.UserService.user.UserRepository;
 import com.ll.exam.UserService.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +12,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class UserServiceTests {
   @Autowired
   private UserService userService;
+  @Autowired
+  private UserRepository userRepository;
+
+  @BeforeEach
+  void beforeEach() {
+    clearData();
+    createSampleData();
+  }
+
+  public static void createSampleData(UserService userService) {
+    userService.create("admin", "admin@test.com", "1234");
+    userService.create("user1", "user1@test.com", "1234");
+  }
+
+  private void createSampleData() {
+    createSampleData(userService);
+  }
+
+  public static void clearData(UserRepository userRepository) {
+    userRepository.deleteAll(); // DELETE FROM site_user;
+    userRepository.truncateTable();
+  }
+
+  private void clearData() {
+    clearData(userRepository);
+  }
+
 
   @Test
   @DisplayName("회원가입이 가능하다.")
   public void t1() {
-    userService.create("user1", "user1@email.com", "1234");
+    userService.create("user2", "user2@email.com", "1234");
   }
 }
