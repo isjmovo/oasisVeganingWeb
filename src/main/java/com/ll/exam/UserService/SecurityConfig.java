2628afd222d99b7564fb83284571f24b5bdae72d
isjmovo <isjmovo@naver.com>
2023-06-11 17:48:50 +0900

    로그인 폼 구현

--- src/main/java/com/ll/exam/UserService/SecurityConfig.java
+++ src/main/java/com/ll/exam/UserService/SecurityConfig.java
@@ -23,7 +23,11 @@ public class SecurityConfig {
         .and()
         .headers()
         .addHeaderWriter(new XFrameOptionsHeaderWriter(
-            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+        .and()
+        .formLogin()
+        .loginPage("/user/login")
+        .defaultSuccessUrl("/");
 
     return http.build();
   }
