f132cdfb48aec33728053f96e9a63909fb0505ee
isjmovo <isjmovo@gmail.com>
2023-08-12 19:14:34 +0900

    스마트렌즈 페이지 생성

---
+++ src/main/java/com/ll/exam/UserService/lens/LensController.java
@@ -0,0 +1,14 @@
+package com.ll.exam.UserService.lens;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lens")
+public class LensController {
+  @GetMapping
+  public String lens() {
+    return "smartLens";
+  }
+}
