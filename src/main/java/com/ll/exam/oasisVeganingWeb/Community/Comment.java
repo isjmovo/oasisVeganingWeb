9c717641fd4946c38f0ec7d887356b8dc84fcc0b
isjmovo <isjmovo@gmail.com>
2023-08-29 14:06:51 +0900

    Community와 Comment 엔티티 추가

---
+++ src/main/java/com/ll/exam/oasisVeganingWeb/Community/Comment.java
@@ -0,0 +1,23 @@
+package com.ll.exam.oasisVeganingWeb.Community;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Comment {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(columnDefinition = "TEXT")
+  private String content;
+
+  private LocalDateTime createDate;
+
+  private Community community;
+}
