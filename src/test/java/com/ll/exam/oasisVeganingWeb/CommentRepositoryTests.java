1557402ae4a42b1b5f8b866381d48d7ab21e23d2
isjmovo <isjmovo@gmail.com>
2023-08-29 15:57:08 +0900

    commentRepository 저장 테스트 케이스 추가

---
+++ src/test/java/com/ll/exam/oasisVeganingWeb/CommentRepositoryTests.java
@@ -0,0 +1,29 @@
+package com.ll.exam.oasisVeganingWeb;
+
+import com.ll.exam.oasisVeganingWeb.Community.Comment;
+import com.ll.exam.oasisVeganingWeb.Community.CommentRepository;
+import com.ll.exam.oasisVeganingWeb.Community.Community;
+import com.ll.exam.oasisVeganingWeb.Community.CommunityRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class CommentRepositoryTests {
+  @Autowired
+  private CommunityRepository communityRepository;
+  @Autowired
+  private CommentRepository commentRepository;
+
+  @Test
+  void 저장() {
+    Community c = communityRepository.findById(2).get();
+    Comment m = new Comment();
+    m.setContent("맛있을 것 같아요!");
+    m.setCommunity(c);
+    m.setCreateDate(LocalDateTime.now());
+    commentRepository.save(m);
+  }
+}
